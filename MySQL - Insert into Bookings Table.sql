70763c5489689c989759cba14780119c8e364936
torislin <linderrai@gmail.com>
2023-11-30 19:00:30 +0100

    Adding SQL-file

---
+++ MySQL - Insert into Bookings Table.sql
@@ -0,0 +1,5 @@
+INSERT INTO Bookings(BookingID, BookingDate, TableNumber, CustomerID, NumberOfGuests)
+VALUES (1, '2022-10-10', 5, 1, 1), 
+(2, '2022-11-12', 3, 3, 2),
+(3, '2022-10-11', 2, 2, 3),
+(4, '2022-10-13', 2, 1, 4);
